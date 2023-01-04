cff33b590b8202c9f278e3f840eb5be13ce70437
AurelienFT <aurelien.foucault@epitech.eu>
2023-01-04 15:58:17 +0100

    Update code to read multi line env.

--- src/sc_generation/abis.rs
+++ src/sc_generation/abis.rs
@@ -1,10 +1,30 @@
 pub fn get_abis(file_path: &String) -> Vec<Vec<String>> {
     let abis_string =
         std::fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let mut abis: Vec<Vec<String>> = abis_string
-        .lines()
+    let content: Vec<&str> = abis_string.lines().collect();
+    // Group prototypes splitted in multiple lines
+    let mut abis_string_vec: Vec<String> = Vec::new();
+    let mut in_abi_definition = false;
+
+    for line in content {
+        if line.starts_with("  export declare function") {
+            in_abi_definition = true;
+            abis_string_vec.push(line.to_string());
+        } else if in_abi_definition {
+            if line.starts_with("    ") || line.ends_with(";") {
+                abis_string_vec
+                    .last_mut()
+                    .unwrap()
+                    .push_str(&format!(" {}", &line.trim_start()));
+            } else {
+                in_abi_definition = false;
+            }
+        }
+    }
+
+    let mut abis: Vec<Vec<String>> = abis_string_vec.iter()
         .filter_map(|line| {
-            if line.starts_with("    export declare function") {
+            if line.starts_with("  export declare function") {
                 Some(
                     line.trim_start()
                         .split("export declare function ")
@@ -16,10 +36,10 @@ pub fn get_abis(file_path: &String) -> Vec<Vec<String>> {
             }
         })
         .map(|line| &line[0..line.len() - 1])
-        .map(|function| function.split('(').map(String::from).collect())
+        .map(|function| function.split('(').map(|s| String::from(s.trim_start())).collect())
         .map(|function_array: Vec<String>| {
             let mut abi = vec![function_array[0].clone()];
-            abi.extend(function_array[1].split("): ").map(String::from));
+            abi.extend(function_array[1].split("): ").map(|s| String::from(s.trim_end_matches(&[',', ' ']))));
             abi
         })
         .map(|function_array| {
@@ -31,5 +51,6 @@ pub fn get_abis(file_path: &String) -> Vec<Vec<String>> {
         .collect();
     abis.push(vec![String::from("seed")]);
     abis.push(vec![String::from("Date.now")]);
+
     abis
 }
