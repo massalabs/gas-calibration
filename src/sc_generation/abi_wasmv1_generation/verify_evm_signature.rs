4703e783f34a4ef30f2e05691a4a1fdb7bd6e295
Leo-Besancon <lb@massa.net>
2023-07-13 08:44:56 +0200

    fmt

--- src/sc_generation/abi_wasmv1_generation/verify_evm_signature.rs
+++ src/sc_generation/abi_wasmv1_generation/verify_evm_signature.rs
@@ -2,7 +2,8 @@ use super::static_evm_triplet;
 
 pub fn generate_abi_verify_evm_signature(calls: &mut Vec<String>) {
     let (message, signature, public_key) = static_evm_triplet();
-    calls.push(format!("{{let message = new Uint8Array({});
+    calls.push(format!(
+        "{{let message = new Uint8Array({});
         message.set({:?});
         let signature = new Uint8Array({});
         signature.set({:?});
